54c3dcbe303985c0cf77903f4bc72cbb031bf8b9
Vipul Gupta <vipulgupta92998@gmail.com>
2026-07-30 22:32:03 +0530

    fully completed and optimised solution

--- Best_time_to_buy_stock.java
+++ Best_time_to_buy_stock.java
@@ -2,20 +2,34 @@ package pkg;
 
 public class Best_time_to_buy_stock {
     public static void main(String[] args) {
-        int [] prices = {7,1,5,3,6,4};
+        int[] prices = { 7, 1, 5, 3, 6, 4 };
         int profit = maxProfit(prices);
         System.out.println(profit);
     }
 
     static int maxProfit(int[] prices) {
+        int minNum = prices[0];
         int maxpr = 0;
         for (int i = 0; i < prices.length; i++) {
-            for (int j = i; j < prices.length; j++) {
-                if (prices[j]-prices[i] > maxpr){
-                    maxpr = prices[j]-prices[i] ;
-                }
+            if (prices[i] < minNum) {
+                minNum = prices[i];
+            }
+            int profit = prices[i] - minNum;
+            if (profit > maxpr){
+                maxpr = profit;
             }
         }
         return maxpr;
     }
+    // static int maxProfit(int[] prices) {
+    // int maxpr = 0;
+    // for (int i = 0; i < prices.length; i++) {
+    // for (int j = i; j < prices.length; j++) {
+    // if (prices[j] - prices[i] > maxpr) {
+    // maxpr = prices[j] - prices[i];
+    // }
+    // }
+    // }
+    // return maxpr;
+    // }
 }
