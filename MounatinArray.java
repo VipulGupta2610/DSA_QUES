efd02469f7c8020a03019dd6bcab42f185565018
Vipul Gupta <vipulgupta92998@gmail.com>
2026-07-31 23:29:20 +0530

    tried pow que but was unablw to solve

---
+++ MounatinArray.java
@@ -0,0 +1,16 @@
+public class MounatinArray {
+    public static void main(String[] args) {
+        int[] arr = { 0, 1, 0 };
+    }
+
+    static int peakIndexInMountainArray(int[] arr) {
+        int idx = helper(arr, 0, arr.length-1);
+        return idx;
+    }
+
+    static int helper(int[] arr, int start, int end) {
+        while (start < end) {
+            
+        }
+    }
+}
