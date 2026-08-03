f5b6e3dc9c2f89476ad7f0e77c0bb40a25466c6f
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-03 23:43:19 +0530

    base ready now making logics

---
+++ SingleNumber_in_DupicatNumbers_Array.java
@@ -0,0 +1,14 @@
+package pkg;
+
+public class SingleNumber_in_DupicatNumbers_Array {
+    public static void main(String[] args) {
+        int [] arr = {1,1,2,3,3,4,4,8,8};
+int ans = singleNonDuplicate(arr);
+System.out.println(ans);
+    }
+
+    static int singleNonDuplicate(int[] nums) {
+
+    }
+
+}
