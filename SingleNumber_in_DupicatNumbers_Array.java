91f1a045956b5276bf85c2823811fc108c20c6e2
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-03 23:45:36 +0530

    tested for different cases

--- SingleNumber_in_DupicatNumbers_Array.java
+++ SingleNumber_in_DupicatNumbers_Array.java
@@ -3,8 +3,14 @@ package pkg;
 public class SingleNumber_in_DupicatNumbers_Array {
     public static void main(String[] args) {
         int[] arr = { 1, 1, 2, 3, 3, 4, 4, 8, 8 };
+        int[] arr1 = { 1, 1, 2,2, 3, 3, 4, 8, 8 };
+        int[] arr2 = { 1, 1, 2,2, 3, 3, 4, 4, 8 };
         int ans = singleNonDuplicate(arr);
+        int ans1 = singleNonDuplicate(arr1);
+        int ans2 = singleNonDuplicate(arr2);
         System.out.println(ans);
+        System.out.println(ans1);
+        System.out.println(ans2);
     }
 
     static int singleNonDuplicate(int[] nums) {
