87eb13b4a6b80ab56f638aaaf577aea0b4d7d2b9
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-06 00:12:46 +0530

    error in main logic fixing it

---
+++ ValidAnagram.java
@@ -0,0 +1,20 @@
+package pkg;
+
+public class ValidAnagram {
+
+    public static void main(String[] args) {
+
+    }
+
+    static boolean isAnagram(String s, String t) {
+        boolean unique = true;
+        int count = 0;
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+        for (char chr : sArr) {
+            for (int i = 0; i < tArr.length; i++) {
+                
+            }
+        }
+    }
+}
