25073711dfa2937fd318e04030b75bd2a8d60ba5
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-07 22:43:39 +0530

    brute force sollution given

--- Median_of_two_arrays.java
+++ Median_of_two_arrays.java
@@ -10,5 +10,23 @@ public class Median_of_two_arrays {
         double ans = findMedianSortedArrays(nums2, nums2);
     }
 
-
+    static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int[] merged = new int[m + n];
+        int n1idx = m - 1;
+        int n2idx = n - 1;
+        for (int i = merged.length - 1; i >= 0; i--) {
+            if (nums1[n1idx] > nums2[n2idx]) {
+                merged[i] = nums1[n1idx];
+                n1idx -= 1;
+            }
+            else {
+                merged[i] = nums2[n2idx];
+                n2idx--;
+            }
+        }
+        System.out.println(Arrays.toString(merged));
+        return 0.0;
+    }
 }
