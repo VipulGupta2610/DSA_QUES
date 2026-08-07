66571278d45eec618d34389cbe2b54d06c68b11a
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-07 22:43:16 +0530

    que attempting

---
+++ Median_of_two_arrays.java
@@ -0,0 +1,14 @@
+package pkg;
+
+import java.util.Arrays;
+
+
+public class Median_of_two_arrays {
+    public static void main(String[] args) {
+        int [] nusm1 = {1,2,5,4};
+        int [] nums2 = {3,6,7};
+        double ans = findMedianSortedArrays(nums2, nums2);
+    }
+
+
+}
--- ValidAnagram.java
+++ ValidAnagram.java
@@ -3,7 +3,9 @@ package pkg;
 public class ValidAnagram {
 
     public static void main(String[] args) {
-
+        String t = "nagaram";
+        String s = "anagram";
+        System.out.println(isAnagram(s, t));
     }
 
     static boolean isAnagram(String s, String t) {
@@ -13,8 +15,12 @@ public class ValidAnagram {
         char[] tArr = t.toCharArray();
         for (char chr : sArr) {
             for (int i = 0; i < tArr.length; i++) {
-                
+                count ^= chr^tArr[i];
             }
         }
+        if (count != 0){
+            return true;
+        }
+        return false;
     }
 }
