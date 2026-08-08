c9b10726144d138cf7a6f52856275d91f2f3ac54
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-08 21:52:15 +0530

    magic no que attmepitng

--- MagicNo.java
+++ MagicNo.java
@@ -1,3 +1,11 @@
 public class MagicNo {
-    
+    public static void main(String[] args) {
+        
+    }
+    static void magic(int n ){
+        int ans = 0;
+        while (condition) {
+            
+        }
+    }
 }
