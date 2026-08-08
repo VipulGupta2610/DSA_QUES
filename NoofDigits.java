79dba569cefe2e055b671a9ab4b7af9937b7f278
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-08 22:14:31 +0530

    que solved and test cases passed

--- NoofDigits.java
+++ NoofDigits.java
@@ -1,5 +1,10 @@
+package pkg;
+
 public class NoofDigits {
     public static void main(String[] args) {
-        
+        int n = 10;
+        int b = 2;
+        int ans = (int)(Math.log(n)/Math.log(b))+1;
+        System.out.println(ans);
     }
 }
