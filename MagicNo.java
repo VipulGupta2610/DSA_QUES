df7596359b3fd1cd5d8c7d1af789d8c818291d00
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-08 22:01:24 +0530

    problem solved test cases passed

--- MagicNo.java
+++ MagicNo.java
@@ -1,11 +1,18 @@
+package pkg;
+
 public class MagicNo {
     public static void main(String[] args) {
-        
+        magic(6);
     }
     static void magic(int n ){
         int ans = 0;
-        while (condition) {
-            
+        int base = 5;
+        while (n>0) {
+            int last= n&1;
+            ans += last * base;
+            n = n>>1;
+            base = base *5;
         }
+        System.out.println(ans);
     }
 }
