11f7a922821e395381c4b36bf2f5e604f2fdc27e
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-13 23:36:58 +0530

    done for postive values

--- pow_x_n.java
+++ pow_x_n.java
@@ -2,44 +2,52 @@ package pkg;
 
 public class pow_x_n {
     public static void main(String[] args) {
-        double ans = myPow(2.0, -3);
+        double ans = myPow(2.0, 4);
         System.out.println(ans);
     }
 
     static double myPow(double x, int n) {
         int ans = 1;
         double base = x;
-        if (n<0){
-
+        if (n < 0) {
+            while () {
+                
+            }
+        } else {
+            while (n > 0) {
+                if ((n & 1) == 1) {
+                    ans *= base;
+                }
+                base *= base;
+                n = n >> 1;
+            }
         }
-        else{
-            
-        }
+        return ans;
     }
 }
 // package pkg;
 
 // public class pow_x_n {
-//     public static void main(String[] args) {
-//         double ans = myPow(2.0, -3);
-//         System.out.println(ans);
-//     }
+// public static void main(String[] args) {
+// double ans = myPow(2.0, -3);
+// System.out.println(ans);
+// }
 
-//     static double myPow(double x, int n) {
-//         double sol = 1;
-//         if (n > 0) {
-//             System.out.println("Entered if area");
-//             for (int i = 1; i <= n; i++) {
-//                 sol = sol * x;
-//             }
-//         } else {
-//             int num_after_divided = 1;
-//             System.out.println("Entered else area");
-//               for (int i = 1; i <= (-n); i++) {
-//                 sol = sol * x;
-//             }
-//             return num_after_divided/sol;
-//         }
-//         return sol;
-//     }
+// static double myPow(double x, int n) {
+// double sol = 1;
+// if (n > 0) {
+// System.out.println("Entered if area");
+// for (int i = 1; i <= n; i++) {
+// sol = sol * x;
+// }
+// } else {
+// int num_after_divided = 1;
+// System.out.println("Entered else area");
+// for (int i = 1; i <= (-n); i++) {
+// sol = sol * x;
+// }
+// return num_after_divided/sol;
+// }
+// return sol;
+// }
 // }
