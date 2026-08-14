5a4f6da3ae338a4c1ab0c44dc5cae6b5a1012c93
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-14 23:10:15 +0530

    logiclly correct bt not getting correct output

--- FlipAndInvertImage.java
+++ FlipAndInvertImage.java
@@ -1,5 +1,31 @@
+package pkg;
+
+import java.util.Arrays;
+
 public class FlipAndInvertImage {
     public static void main(String[] args) {
-        
+        int[][] image = { { 1, 1, 0 }, { 1, 0, 1 }, { 0, 0, 0 } };
+        System.out.println(Arrays.deepToString(image));
+        int[][] ans = flipAndInvertImage(image);
+        System.out.println(Arrays.deepToString(ans));
+    }
+
+    static int[][] flipAndInvertImage(int[][] image) {
+        for (int i = 0; i < image.length; i++) {
+            int start = 0;
+            int end = image[i].length - 1;
+            System.out.println(Arrays.toString(image[i]));
+            while (start <= end) {
+                int temp = image[i][start];
+                image[i][start] = image[i][end];
+                image[i][end] = temp;
+                image[i][start] ^= 1;
+                image[i][end] ^= 1;
+                start++;
+                end--;
+            }
+            System.out.println(Arrays.toString(image[i]));
+        }
+        return image;
     }
 }
