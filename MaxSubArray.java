2c86868d0fa3387e91612cc056ee2c5f1acfd578
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-16 23:23:28 +0530

    trying to solve new question

--- MaxSubArray.java
+++ MaxSubArray.java
@@ -3,6 +3,7 @@ package pkg;
 public class MaxSubArray {
 
     public static void main(String[] args) {
-
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
     }
+    
 }
---
+++ minimum_of_maximum_ans.java
@@ -0,0 +1,7 @@
+package pkg;
+
+public class minimum_of_maximum_ans {
+    public static void main(String[] args) {
+        
+    }
+}
