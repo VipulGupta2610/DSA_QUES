9f50dc8af28635f2751d252f9b206736936e0489
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-16 23:39:10 +0530

    first step passed

--- minimum_of_maximum_ans.java
+++ minimum_of_maximum_ans.java
@@ -2,6 +2,23 @@ package pkg;
 
 public class minimum_of_maximum_ans {
     public static void main(String[] args) {
-        
+        int[] arr = { 11, 6 };
+        int n = 6;
+        int ans = minimizedMaximum(n, arr);
+    }
+
+    static int minimizedMaximum(int n, int[] quantities) {
+        int min = 0;
+        int max = maxItem(quantities);
+        System.out.println(max);
+        return max;
+    }
+
+    static int maxItem(int[] arr) {
+        int num = 0;
+        for (int i = 0; i < arr.length; i++) {
+            num += arr[i];
+        }
+        return num;
     }
 }
