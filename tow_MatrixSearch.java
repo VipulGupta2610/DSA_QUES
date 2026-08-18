d8d9ee51a60d3a60f6f1f7c45dca0057af031cf6
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-18 22:54:12 +0530

    base ready

---
+++ tow_MatrixSearch.java
@@ -0,0 +1,14 @@
+package pkg;
+
+public class tow_MatrixSearch {
+
+    public static void main(String[] args) {
+        int [][] matrix = {{1,3,5,7},{9,11,13,15},{17,19,21,23}};
+        int target = 13;
+        boolean isThere = searchMatrix(matrix, target);
+        
+    }
+    static boolean searchMatrix(int[][] matrix, int target) {
+        
+    }
+}
