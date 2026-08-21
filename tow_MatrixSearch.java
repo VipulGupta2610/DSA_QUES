b1821c0764c6b119c2a1bd59e1201be8702ef716
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-21 22:35:05 +0530

    using another approah for more optimistic solution

--- tow_MatrixSearch.java
+++ tow_MatrixSearch.java
@@ -9,28 +9,28 @@ public class tow_MatrixSearch {
         System.out.println(isThere);
     }
 
-    static boolean searchMatrix(int[][] matrix, int target) {
-        int[] duplicate = new int[matrix.length * (matrix[0].length)];
-        // creating duplicate array is not allowed but it was working fine 
-        // int d_idx = 0;
-        // for (int i = 0; i < matrix.length; i++) {
-        //     for (int j = 0; j < matrix[i].length; j++) {
-        //         duplicate[d_idx] = matrix[i][j];
-        //         d_idx++;
-        //     }
-        // }
-        int start = 0;
-        int end = duplicate.length - 1;
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-            if (duplicate[mid] == target) {
-                return true;
-            } else if (target > duplicate[mid]) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        return false;
-    }
+    // static boolean searchMatrix(int[][] matrix, int target) {
+    //     int[] duplicate = new int[matrix.length * (matrix[0].length)];
+    //     // creating duplicate array is not allowed but it was working fine 
+    //     // int d_idx = 0;
+    //     // for (int i = 0; i < matrix.length; i++) {
+    //     //     for (int j = 0; j < matrix[i].length; j++) {
+    //     //         duplicate[d_idx] = matrix[i][j];
+    //     //         d_idx++;
+    //     //     }
+    //     // }
+    //     int start = 0;
+    //     int end = duplicate.length - 1;
+    //     while (start < end) {
+    //         int mid = start + (end - start) / 2;
+    //         if (duplicate[mid] == target) {
+    //             return true;
+    //         } else if (target > duplicate[mid]) {
+    //             start = mid + 1;
+    //         } else {
+    //             end = mid - 1;
+    //         }
+    //     }
+    //     return false;
+    // }
 }
