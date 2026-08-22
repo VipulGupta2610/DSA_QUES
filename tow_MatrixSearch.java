a475423e74cd2e18dc59c0d88e76dd658f08e16f
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-22 22:59:36 +0530

    testing new approach

--- tow_MatrixSearch.java
+++ tow_MatrixSearch.java
@@ -5,14 +5,68 @@ public class tow_MatrixSearch {
     public static void main(String[] args) {
         int[][] matrix = { { 1, 3, 5, 7 }, { 9, 11, 13, 15 }, { 17, 19, 21, 23 } };
         int target = 13;
-        System.out.println(matrix[0].length-1);
+        System.out.println(matrix[0].length - 1);
         boolean isThere = searchMatrix(matrix, target);
         System.out.println(isThere);
     }
 
     static boolean searchMatrix(int[][] matrix, int target) {
-        
+        int rStart = 0;
+        int rEnd = matrix.length - 1;
+        int cStart = 0;
+        int cEnd = matrix[0].length - 1;
+        while (cStart <= cEnd) {
+            int midRow = rStart + (rEnd - rStart) / 2;
+            if (target >= matrix[midRow][cStart] && target <= matrix[midRow][cEnd]) {
+                return simpleBinarySearch(matrix, target, midRow, cStart, cEnd);
+            } else if (target > matrix[midRow][cEnd]) {
+                rStart = midRow;
+            } else {
+                rEnd = midRow;
+            }
+        }
+        return false;
     }
+
+    static boolean simpleBinarySearch(int[][] matrix, int target, int row, int start, int end) {
+        while (start<=end) {
+            int mid = start + (end - start)/2;
+            if (target==matrix[row][mid]){
+                return true;
+            }
+            else if (target>matrix[row][mid]){
+                start = mid;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return false;
+    }
+    // static boolean searchMatrix(int[][] matrix, int target) {
+    // int divider = matrix.length;
+    // int row = 0;
+    // int maxRow = matrix.length - 1;
+    // int column = 0;
+    // int maxColumn = matrix[0].length - 1;
+    // while (row <= maxRow && column <= maxColumn && row>=0 && column>=0) {
+    // int midRow = row + (maxRow - row) / 2;
+    // int midCol = column + (maxColumn - column) / 2;
+    // System.out.println("Mid row : "+midRow);
+    // System.out.println("Mid col : "+midCol);
+    // if (matrix[midRow][midCol] == target) {
+    // return true;
+    // }
+    // if (target < matrix[midRow][maxColumn]) {
+    // maxRow = midRow;
+    // maxColumn = midCol;
+    // } else {
+    // row=midRow;
+    // column=midCol;
+    // }
+    // }
+    // return false;
+    // }
 
     // static boolean searchMatrix(int[][] matrix, int target) {
     // int[] duplicate = new int[matrix.length * (matrix[0].length)];
