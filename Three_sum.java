a406825eddd532c84b1719e26ea02915d226fabb
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-26 22:25:28 +0530

    trying brute force approach

--- Three_sum.java
+++ Three_sum.java
@@ -1,10 +1,30 @@
 package pkg;
+
+import java.util.ArrayList;
+
 public class Three_sum {
     public static void main(String[] args) {
-        
+
     }
 
-    static List<List<Integer>> threeSum(int[] nums) {
-        
+    static ArrayList<ArrayList<Integer>> threeSum(int[] nums) {
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int j = 0;
+            for (j = 0; j < nums.length; j++) {
+                if (i == j){
+                    continue;
+                }
+                int j2 = 0;
+                for (j2 = 0; j2 < nums.length; j2++) {
+                    if (i == j && j  == j2){
+                        continue;
+                    }
+                    if (nums[i] + nums[j] + nums[k] == 0){
+                        list.add(null)
+                    }
+                }
+            }
+        }
     }
 }
