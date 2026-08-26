85355d98aef3a6e1c0ed957e01a296e0dc9a2b52
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-26 22:31:23 +0530

    first try

--- Three_sum.java
+++ Three_sum.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 
 public class Three_sum {
     public static void main(String[] args) {
-
+        int[] arr = { -1, 0, 1, 2, -1, -4 };
+ArrayList<ArrayList<Integer>> ans = threeSum(arr);
+System.out.println(ans);
     }
 
     static ArrayList<ArrayList<Integer>> threeSum(int[] nums) {
@@ -12,19 +14,24 @@ public class Three_sum {
         for (int i = 0; i < nums.length; i++) {
             int j = 0;
             for (j = 0; j < nums.length; j++) {
-                if (i == j){
+                if (i == j) {
                     continue;
                 }
                 int j2 = 0;
                 for (j2 = 0; j2 < nums.length; j2++) {
-                    if (i == j && j  == j2){
+                    if (i == j || i == j2 || j == j2) {
                         continue;
                     }
-                    if (nums[i] + nums[j] + nums[k] == 0){
-                        list.add(null)
+                    if (nums[i] + nums[j] + nums[j2] == 0) {
+                        ArrayList<Integer> triplet = new ArrayList<>();
+                        triplet.add(nums[i]);
+                        triplet.add(nums[j]);
+                        triplet.add(nums[j2]);
+                        list.add(triplet);
                     }
                 }
             }
         }
+        return list;
     }
 }
