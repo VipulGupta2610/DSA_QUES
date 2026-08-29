80055501f9810a327e91a4d383b08792b5b86088
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-29 22:26:34 +0530

    working on new approach

--- Three_sum.java
+++ Three_sum.java
@@ -17,6 +17,18 @@ public class Three_sum {
 
             for (int j = i + 1; j < nums.length; j++) {
 
+                
+            }
+        }
+        return list;
+    }
+    // static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
                 for (int j2 = j + 1; j2 < nums.length; j2++) {
 
                     if (nums[i] + nums[j] + nums[j2] == 0) {
