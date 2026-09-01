90d68ad29dec1bf5fe23294f664728bb03e07fee
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-01 23:01:35 +0530

    testing the approach

--- Three_sum.java
+++ Three_sum.java
@@ -20,120 +20,89 @@ public class Three_sum {
             sortedarray.add(nums[i]);
         }
 
-        int n = nums.length-1;
+        int n = nums.length - 1;
 
         HashSet<List<Integer>> list = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
-            int j = i+1;
+            int j = i + 1;
             int k = n;
-            while (j<k) {
-                if (j>0 && sortedarray.get(j)==sortedarray.get(j-1)){
+            while (j < k) {
+                if (j > 0 && sortedarray.get(j) == sortedarray.get(j - 1)) {
                     continue;
                 }
-                
+                if (sortedarray.get(i) + sortedarray.get(j) + sortedarray.get(k) == 0) {
+                    ArrayList<Integer> triplet = new ArrayList<>();
+                    triplet.add(sortedarray.get(i));
+                    triplet.add(sortedarray.get(j));
+                    triplet.add(sortedarray.get(k));
+                    list.add(triplet);
+                    while (j<k && sortedarray.get(j)==sortedarray.get(j-1)) {
+                        j++;
+                    }
+                }
+                else if (sortedarray.get(i)+sortedarray.get(j)+sortedarray.get(k)>0){
+                    k--;
+                }else{
+                    j++;
+                }
             }
-        }   
+        }
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // static List<List<Integer>> threeSum(int[] nums) {
-//     List<List<Integer>> list = new ArrayList<>();
-//     for (int i = 0; i < nums.length; i++) {
+// List<List<Integer>> list = new ArrayList<>();
+// for (int i = 0; i < nums.length; i++) {
 
-//         int target = -nums[i];
+// int target = -nums[i];
 
-//         HashSet<Integer> set = new HashSet<>();
+// HashSet<Integer> set = new HashSet<>();
 
-//         for (int j = i+1; j < nums.length; j++) {
+// for (int j = i+1; j < nums.length; j++) {
 
-//             int third = target-nums[j];
-            
-//             if (set.contains(third)){
-//                 ArrayList<Integer> triplet = new ArrayList<>();
-//                 triplet.add(nums[i]);
-//                 triplet.add(nums[j]);
-//                 triplet.add(third);
-//                 triplet.sort(null);
-//                 if (!list.contains(triplet)){
-//                     list.add(triplet);
-//                 }
-//             }
-//             set.add(nums[j]);
-//         }
-//     }
-//     return list;
+// int third = target-nums[j];
+
+// if (set.contains(third)){
+// ArrayList<Integer> triplet = new ArrayList<>();
+// triplet.add(nums[i]);
+// triplet.add(nums[j]);
+// triplet.add(third);
+// triplet.sort(null);
+// if (!list.contains(triplet)){
+// list.add(triplet);
+// }
+// }
+// set.add(nums[j]);
+// }
+// }
+// return list;
 // }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // static List<List<Integer>> threeSum(int[] nums) {
-//     List<List<Integer>> list = new ArrayList<>();
+// List<List<Integer>> list = new ArrayList<>();
 
-//     for(
-//     int i = 0;i<nums.length;i++)
-//     {
+// for(
+// int i = 0;i<nums.length;i++)
+// {
 
-//         for (int j = i + 1; j < nums.length; j++) {
+// for (int j = i + 1; j < nums.length; j++) {
 
-//             for (int j2 = j + 1; j2 < nums.length; j2++) {
+// for (int j2 = j + 1; j2 < nums.length; j2++) {
 
-//                 if (nums[i] + nums[j] + nums[j2] == 0) {
-//                     ArrayList<Integer> triplet = new ArrayList<>();
-//                     triplet.add(nums[i]);
-//                     triplet.add(nums[j]);
-//                     triplet.add(nums[j2]);
-//                     triplet.sort(null);
-//                     if (list.contains(triplet)) {
-//                         continue;
-//                     }
-//                     list.add(triplet);
-//                 }
-//             }
-//         }
-//     }return list;
+// if (nums[i] + nums[j] + nums[j2] == 0) {
+// ArrayList<Integer> triplet = new ArrayList<>();
+// triplet.add(nums[i]);
+// triplet.add(nums[j]);
+// triplet.add(nums[j2]);
+// triplet.sort(null);
+// if (list.contains(triplet)) {
+// continue;
+// }
+// list.add(triplet);
+// }
+// }
+// }
+// }return list;
 // }
 // static List<List<Integer>> threeSum(int[] nums) {
 // List<List<Integer>> list = new ArrayList<>();
