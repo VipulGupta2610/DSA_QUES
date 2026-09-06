1e3fc1ef80c7228c8fb58428d2ab3839ffe12807
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-06 23:28:32 +0530

    first try

--- Product_of_array.java
+++ Product_of_array.java
@@ -1,11 +1,24 @@
 package pkg;
 
+import java.util.Arrays;
+
 public class Product_of_array {
     public static void main(String[] args) {
-        
+        int [] arr = {1,2,3,4};
+        int [] ans = productExceptSelf(arr);
+        System.out.println(Arrays.toString(ans));
     }
 
     static  int[] productExceptSelf(int[] nums) {
-        
+        int [] anss = new int[nums.length];
+        int totalPrd=1;
+        for (int i = 0; i < anss.length; i++) {
+            totalPrd*=nums[i];
+        }
+        for (int i = 0; i < anss.length; i++) {
+            int ans = totalPrd/nums[i];
+            anss[i]=ans;
+        }
+        return anss;
     }
 }
