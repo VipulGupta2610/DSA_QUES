3b5270c0f49cfec20000be13f1f19d93fda114e8
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-07 22:42:38 +0530

    changing the approach

--- Product_of_array.java
+++ Product_of_array.java
@@ -4,14 +4,15 @@ import java.util.Arrays;
 
 public class Product_of_array {
     public static void main(String[] args) {
-        int [] arr = {-1,1,0,-3,3};
+        // int [] arr = {-1,1,0,-3,3};
+        int [] arr = {0,0};
         int [] ans = productExceptSelf(arr);
         System.out.println(Arrays.toString(ans));
     }
 
     static  int[] productExceptSelf(int[] nums) {
         int [] anss = new int[nums.length];
-        int totalPrd=1;
+        int totalPrd = 1;
         int final_prd = 1;
         for (int i = 0; i < anss.length; i++) {
             totalPrd*=nums[i];
