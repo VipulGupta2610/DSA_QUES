a95251d73e1bd7216710bf93c8744cf693efb1ad
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-08 23:32:56 +0530

    testing new approach

--- ValidMountainArr.java
+++ ValidMountainArr.java
@@ -2,29 +2,40 @@ package pkg;
 
 public class ValidMountainArr {
     public static void main(String[] args) {
-        int[] arr = { 0, 2, 1, 0 };
-        int IsMountain = validMountainArray(arr);
+        // int[] arr = { 0, 2, 1, 0};
+        int[] arr = { 1, 2, 3, 4, 8, 9, 10 };
+        boolean IsMountain = validMountainArray(arr);
         System.out.println(IsMountain);
     }
 
-    static int validMountainArray(int[] arr) {
+    static boolean validMountainArray(int[] arr) {
+
+        int peak = findPeak(arr);
+        System.out.println(arr[peak]);
+        if (peak == 0 || peak == arr.length-1){
+            return false;
+        }
+        return peak;
+
+    }
+
+    static int findPeak(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
         int mid;
-        boolean mnArr;
+
         while (start < end) {
-            mid = start + (end - start)/2;
-            if (mid > 0 && mid < arr.length-1 && arr[mid]>arr[mid+1]&&arr[mid]>arr[mid-1]){
-                return arr[mid];
+            mid = start + (end - start) / 2;
+            if (mid > 0 && mid < arr.length - 1 && arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]) {
+                return mid;
             }
-            if (arr[mid]>arr[mid+1]){
+            if (arr[mid] > arr[mid + 1]) {
                 end = mid;
+            } else if (arr[mid] > arr[mid - 1]) {
+                start = mid+1;
             }
-            else if (arr[mid]>arr[mid-1]){
-                start = mid;
-            }
-            
+
         }
-        return arr[start];
+        return end;
     }
 }
