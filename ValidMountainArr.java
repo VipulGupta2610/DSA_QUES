2e0b1f8d25c3ca2f3640559ef2c67209b21702a8
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-08 23:24:32 +0530

    working on new approach

--- ValidMountainArr.java
+++ ValidMountainArr.java
@@ -3,18 +3,28 @@ package pkg;
 public class ValidMountainArr {
     public static void main(String[] args) {
         int[] arr = { 0, 2, 1, 0 };
-        boolean IsMountain = validMountainArray(arr);
+        int IsMountain = validMountainArray(arr);
         System.out.println(IsMountain);
     }
 
-    static boolean validMountainArray(int[] arr) {
+    static int validMountainArray(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
         int mid;
         boolean mnArr;
         while (start < end) {
             mid = start + (end - start)/2;
+            if (mid > 0 && mid < arr.length-1 && arr[mid]>arr[mid+1]&&arr[mid]>arr[mid-1]){
+                return arr[mid];
+            }
+            if (arr[mid]>arr[mid+1]){
+                end = mid;
+            }
+            else if (arr[mid]>arr[mid-1]){
+                start = mid;
+            }
+            
         }
-        return start != 0 && end != arr.length - 1 && start == end;
+        return arr[start];
     }
 }
