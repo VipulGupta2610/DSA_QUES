e4ab3e7b94156cec8f980d017fe62b86971e57a0
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-10 22:18:14 +0530

    started another que

---
+++ validPalindrome.java
@@ -0,0 +1,18 @@
+package pkg;
+
+public class validPalindrome {
+    public static void main(String[] args) {
+
+    }
+
+    static boolean isPalindrome(String s) {
+        s = s.replace(",", "");
+        s = s.replace(" ", "");
+        s = s.replace(":", "");
+        int start = 0;
+        int end = s.length();
+        while (start<=end) {
+            if (s.charAt(end))
+        }
+    }
+}
