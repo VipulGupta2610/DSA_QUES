92bb75e006843a7f1d323a950764ff5b7a0d3315
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-11 23:13:37 +0530

    first try

--- RemoveAllOcuurances.java
+++ RemoveAllOcuurances.java
@@ -2,11 +2,21 @@ package pkg;
 
 public class RemoveAllOcuurances {
     public static void main(String[] args) {
-
+        String ans = "abcdappledb";
+        System.out.println(removeOccurrences(ans, "apple"));
     }
 
     static String removeOccurrences(String s, String part) {
-        
+        if (s.isEmpty()){
+            return s;
+        }
+        String newString="";
+        if (s.startsWith(part)){
+             newString=removeOccurrences(s.substring(part.length()), part);
+        }else{
+             newString=removeOccurrences(s.substring(0), part);
+        }
+        return newString;
     }
 
 }
