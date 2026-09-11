bbd350d71956904b14d10912db8d149543619de0
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-11 23:18:29 +0530

    it worked

--- RemoveAllOcuurances.java
+++ RemoveAllOcuurances.java
@@ -12,12 +12,11 @@ public class RemoveAllOcuurances {
         }
         if (s.startsWith(part)) {
             String newString = removeOccurrences(s.substring(part.length()), part);
-            return s.charAt(0) + newString;
-        } else {
-            String newString = removeOccurrences(s.substring(0), part);
             return s.charAt(part.length())+newString;
+        } else {
+            String newString = removeOccurrences(s.substring(1), part);
+            return s.charAt(0) + newString;
         }
-
     }
 
 }
