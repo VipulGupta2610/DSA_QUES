dce94efbfb249a874000d625c39b29d0d6192496
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-13 22:05:53 +0530

    reworking on the approach

--- RemoveAllOcuurances.java
+++ RemoveAllOcuurances.java
@@ -3,19 +3,20 @@ package pkg;
 public class RemoveAllOcuurances {
     public static void main(String[] args) {
         String ans = "daabcbaabcbc";
-        System.out.println(removeOccurrences(ans, "apple"));
+        System.out.println(removeOccurrences(ans, "abc"));
     }
 
     static String removeOccurrences(String s, String part) {
         if (s.isEmpty()) {
             return s;
         }
+
         if (s.startsWith(part)) {
-            String newString = removeOccurrences(s.substring(part.length()), part);
-            return s.charAt(part.length())+newString;
+            String newString = removeOccurrences(s.substring(part.length() - 1), part);
+            return newString + s.charAt(part.length());
         } else {
             String newString = removeOccurrences(s.substring(1), part);
-            return s.charAt(0) + newString;
+            return newString + s.charAt(0);
         }
     }
 
