93662943682b30d8a247f7b0802256ec02d99eb7
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-14 23:08:12 +0530

    work started

--- RemoveAllOcuurances.java
+++ RemoveAllOcuurances.java
@@ -7,20 +7,7 @@ public class RemoveAllOcuurances {
     }
 
     static String removeOccurrences(String s, String part) {
-        String newstring = s;
-        String ans="";
-        int i = 0;
-        while (i<newstring.length()) {
-            String tString = newstring.substring(i);
-            if(tString.startsWith(part)){
-                newstring += newstring.substring(i, part.length())+newstring.substring(part.length(),newstring.length());
-                i=0;
-            }else{
-                i++;
-            }
-        }
-        ans = newstring;
-        return  ans;
+        
     }
 
 }
