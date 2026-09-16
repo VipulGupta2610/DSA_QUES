48d48e0bd00c16df259bb151ada9bb8d46c20b6e
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-16 22:38:36 +0530

    brute forece done

--- kadaneAlgo.java
+++ kadaneAlgo.java
@@ -2,22 +2,22 @@ package pkg;
 
 public class kadaneAlgo {
     public static void main(String[] args) {
-        int []nums = {1,2,3,4,5};
-maxSubArray(nums);
+        int[] nums = { 1, -1, 3, 4, 5 };
+        int max = maxSubArray(nums);
+        System.out.println(max);
     }
 
     static int maxSubArray(int[] nums) {
         int n = nums.length;
         int maxSum = 0;
         for (int i = 0; i < nums.length; i++) {
-            int currentSum=0;
+            int currentSum = 0;
             for (int j = i; j < n; j++) {
-               currentSum+=nums[j];
-               if (currentSum>maxSum){
-                maxSum=currentSum;
-               }
+                currentSum += nums[j];
+                if (currentSum > maxSum) {
+                    maxSum = currentSum;
+                }
             }
-            
         }
         return maxSum;
     }
