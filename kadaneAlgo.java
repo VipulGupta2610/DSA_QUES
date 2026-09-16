0eeeff42b5a626c79d264b13bd2f1d92a4fd2eeb
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-16 22:59:36 +0530

    done lets test

--- kadaneAlgo.java
+++ kadaneAlgo.java
@@ -10,8 +10,16 @@ public class kadaneAlgo {
     static int maxSubArray(int[] nums) {
        int n = nums.length;
        int maxSum = Integer.MIN_VALUE;
+       int currentSum = 0;
        for (int i = 0; i < n; i++) {
-        
+            currentSum+=nums[i];
+            if (currentSum>maxSum){
+                maxSum=currentSum;
+            }
+            if (currentSum<0){
+                currentSum=0;
+            }
        }
+       return maxSum;
     }
 }
