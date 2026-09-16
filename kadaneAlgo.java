c6268cee67816b2afb3b8cd6d3728c6d24799443
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-16 22:15:36 +0530

    prinitng all the possible sub arrays

--- kadaneAlgo.java
+++ kadaneAlgo.java
@@ -2,10 +2,20 @@ package pkg;
 
 public class kadaneAlgo {
     public static void main(String[] args) {
-
+        int []nums = {1,2,3,4,5};
+maxSubArray(nums);
     }
 
-    static int maxSubArray(int[] nums) {
-
+    static void maxSubArray(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < n-1; j++) {
+                for (int j2 = i; j2 <=j; j2++) {
+                    System.out.println(nums[j2]);
+                }
+                System.out.println(" ");
+            }
+            System.out.println();
+        }
     }
 }
