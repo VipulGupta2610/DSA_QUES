5052088a2aa97cc7a39e6e5a3ad50b96f0539658
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-19 23:09:55 +0530

    first try

--- ContainerWithMostWater.java
+++ ContainerWithMostWater.java
@@ -3,9 +3,31 @@ package pkg;
 public class ContainerWithMostWater {
     public static void main(String[] args) {
         int[] arr = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+        int ans = maxArea(arr);
+        System.out.println(ans);
     }
 
     static int maxArea(int[] height) {
-
+        int max = 0;
+        for (int i = 0; i < height.length; i++) {
+            int maxLine;
+            int maxIdx;
+            for (int j = i + 1; j < height.length; j++) {
+                if (height[j] > maxLine) {
+                    maxLine = height[j];
+                    maxIdx = j;
+                }
+            }
+            int calcmax;
+            if (height[maxIdx] < height[i]) {
+                calcmax = (maxIdx - i) * height[maxIdx];
+            } else {
+                calcmax = (maxIdx - i) * height[i];
+            }
+            if (max < calcmax) {
+                max = calcmax;
+            }
+        }
+        return max;
     }
 }
