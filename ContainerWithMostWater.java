3ca4cf41de01efdb472178679bbab71c46f2e860
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-19 22:44:15 +0530

    base ready

--- ContainerWithMostWater.java
+++ ContainerWithMostWater.java
@@ -2,6 +2,10 @@ package pkg;
 
 public class ContainerWithMostWater {
     public static void main(String[] args) {
-        
+        int[] arr = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+    }
+
+    static int maxArea(int[] height) {
+
     }
 }
