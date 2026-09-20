18afd1f0b68b215411b74cf5271bee152f8a7db4
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-20 23:10:28 +0530

    working on the solution

--- ContainerWithMostWater.java
+++ ContainerWithMostWater.java
@@ -7,20 +7,29 @@ public class ContainerWithMostWater {
         System.out.println(ans);
     }
 
-
-    
+    static int maxAreaOp(int[] height) {
+        int maxWater = 0;
+        int lp = 0;
+        int rp = height.length - 1;
+        while (lp < rp) {
+            int wdt = rp - lp;
+            int hgt = height[lp] < height[rp] ? height[lp] : height[rp];
+            int currentWater = wdt*hgt;
+            maxWater
+        }
+    }
 
     // non- optimistic
     static int maxArea(int[] height) {
         int max = 0;
         for (int i = 0; i < height.length; i++) {
             int leftLine = height[i];
-            for (int j = i+1; j < height.length; j++) {
+            for (int j = i + 1; j < height.length; j++) {
                 int rightLine = height[j];
-                int hgt = leftLine<=rightLine?leftLine:rightLine;
-                int wdt = j-i;
-                int amt = hgt*wdt;
-                if (amt>max){
+                int hgt = leftLine <= rightLine ? leftLine : rightLine;
+                int wdt = j - i;
+                int amt = hgt * wdt;
+                if (amt > max) {
                     max = amt;
                 }
             }
