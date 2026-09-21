208725fb3ed8f68192af4f588c0b3ab254d45369
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-21 22:14:44 +0530

    code ran succesfully

--- PermutationInString.java
+++ PermutationInString.java
@@ -4,19 +4,31 @@ import java.util.ArrayList;
 
 public class PermutationInString {
     public static void main(String[] args) {
-        String s1 = "abc";
-        String s2 = "a";
-        System.out.println(s1.contains(s2));
+        boolean ans = checkInclusion("ab", "eidbaooo");
+        System.out.println(ans);
     }
 
     static boolean checkInclusion(String s1, String s2) {
         ArrayList<String> list = new ArrayList<>();
         list.add("");
-        int n = list.size();
-       for (int j = 0; j < ; j++) {
-         for (int i = 0; i < n; i++) {
-             
-         }
-       }
+        for (int i = 0; i < s1.length(); i++) {
+            char ch = s1.charAt(i);
+            ArrayList<String> local = new ArrayList<>();
+            for (int j = 0; j < list.size(); j++) {
+                String p = list.get(j);
+                for (int k = 0; k <= p.length(); k++) {
+                    String f = p.substring(0, k);
+                    String s = p.substring(k, p.length());
+                    local.add(f + ch + s);
+                }
+            }
+            list = local;
+        }
+        for (String test : list) {
+            if (s2.contains(test)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
