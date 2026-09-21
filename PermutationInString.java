47624db8996f558b7c71a4df705b5f412a36ea0e
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-21 21:53:27 +0530

    attemptin the que

--- PermutationInString.java
+++ PermutationInString.java
@@ -1,5 +1,7 @@
 package pkg;
 
+import java.util.ArrayList;
+
 public class PermutationInString {
     public static void main(String[] args) {
         String s1 = "abc";
@@ -8,6 +10,13 @@ public class PermutationInString {
     }
 
     static boolean checkInclusion(String s1, String s2) {
-        
+        ArrayList<String> list = new ArrayList<>();
+        list.add("");
+        int n = list.size();
+       for (int j = 0; j < ; j++) {
+         for (int i = 0; i < n; i++) {
+             
+         }
+       }
     }
 }
