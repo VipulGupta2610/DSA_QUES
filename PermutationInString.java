187b3b6ba623d44cf09348bd0ccf2e7b15eb9d4a
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-21 21:48:59 +0530

    attempting new permutation que

---
+++ PermutationInString.java
@@ -0,0 +1,9 @@
+package pkg;
+
+public class PermutationInString {
+    public static void main(String[] args) {
+        String s1 = "abc";
+        String s2 = "a";
+        System.out.println(s1.contains(s2));
+    }
+}
