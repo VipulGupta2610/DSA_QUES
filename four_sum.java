6d8a4fcbfec6271c73fb9d2e783d4cc10bb65b3a
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-24 22:26:02 +0530

    attempting new que

---
+++ four_sum.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class four_sum {
+    public static void main(String[] args) {
+
+    }
+
+    static List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int first = nums[i];
+            for (int j = i+1; j < nums.length; j++) {
+                int second = nums[j];
+                
+            }
+        }
+        return list;
+    }
+}
